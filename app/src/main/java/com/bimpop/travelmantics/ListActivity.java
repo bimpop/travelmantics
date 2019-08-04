020766888bd8973a78df9e645fc1db88cd23e666
Abimbola Popoola <abimbolapopoola57@gmail.com>
2019-08-04 19:16:46 +0100

    fix gradle bugs

--- app/src/main/java/com/bimpop/travelmantics/ListActivity.java
+++ app/src/main/java/com/bimpop/travelmantics/ListActivity.java
@@ -3,7 +3,13 @@ package com.bimpop.travelmantics;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class ListActivity extends AppCompatActivity {
+    ArrayList<TravelDeal> deals;
+    private FirebaseDatabase mFirebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
