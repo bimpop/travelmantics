fe84f4bb8a228f8218f9dda313c5ab57c79b0967
Abimbola Popoola <abimbolapopoola57@gmail.com>
2019-08-05 07:43:10 +0100

    setup the new layout

--- app/src/main/java/com/bimpop/travelmantics/ListActivity.java
+++ app/src/main/java/com/bimpop/travelmantics/ListActivity.java
@@ -1,9 +1,13 @@
 package com.bimpop.travelmantics;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DatabaseReference;
@@ -28,5 +32,23 @@ public class ListActivity extends AppCompatActivity {
         LinearLayoutManager dealsLayoutManager =
                 new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         rvDeals.setLayoutManager(dealsLayoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.list_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.insert_menu:
+                Intent intent = new Intent(ListActivity.this, DealActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
